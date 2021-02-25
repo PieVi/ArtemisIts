d344a6f1c90c1922ce4ccb0aa1b338af49a0bbd5
Panos <panvournas@gmail.com>
2021-02-25 12:41:47 +0200

    algorithms - 1

---
+++ test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import csv
+
+class Adjacency(object):
+    def __init__(self,object_id,priority,distance,tier,attempts,hosr):
+        self.object_id=object_id
+        self.priority=priority
+        self.distance=distance
+        self.tier=tier
+        self.attempts=attempts
+        self.hosr=hosr
+
+def createAdjacencyListFromFile(filename):
+    entries = []
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            entry = Adjacency(
+                row['object_id'],
+                row['priority'],
+                row['distance'],
+                row['tier'],
+                row['attempts'],
+                row['hosr']
+            )
+            entries.append(entry)
+    return entries
