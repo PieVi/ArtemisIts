a5f845670bdd4d60580e4f099193695b25b6f094
Panos <panvournas@gmail.com>
2021-02-25 13:07:34 +0200

    queries - 4

--- queries.sql
+++ queries.sql
@@ -1,15 +1,25 @@
+
+-- Question 1
 SELECT EmpId 
 FROM EmployeeDetails
 INTERSECT
 SELECT EmpId
 FROM EmployeesSalary; 
 
-
+-- Question 2
 SELECT *
 FROM EmployeeDetails
 WHERE YEAR(DateOfJoining) = 2020;
 
+-- Question 3
 SELECT FullName, Salary
 FROM EmployeeDetails
 LEFT JOIN EmployeesSalary
-ON EmployeeDetails.EmpId = EmployeesSalary.EmpId;
+ON EmployeeDetails.EmpId = EmployeesSalary.EmpId
+
+-- Question 4
+SELECT *
+FROM EmployeeDetails
+WHERE EmpId IN (
+    SELECT DISTINCT(ManagerId) From EmployeeDetails
+);
